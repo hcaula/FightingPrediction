24124dc511013aaed70a4521d173edb7bea8c7bd
Henrique Caúla <lhtc@cin.ufpe.br>
2019-02-24 11:19:08 -0300

    Depleates shield

--- game.js
+++ game.js
@@ -4,6 +4,7 @@
 
 let lineBottom, lineWidth, lineBegin, lineEnd, lineColor, lineSize;
 let p1x, p1y, p2x, p2y, p1YSpeed, p2YSpeed, playerSize, playerMargin, initialY;
+let p1ShieldRadius, p2ShieldRadius, initialShieldRadius, shieldRate, p1Shielding, p2Shielding;
 let gravitySpeed, jumpSpeed;
 
 /*
@@ -26,6 +27,10 @@ function setup() {
     p2x = (lineEnd - lineSize * playerMargin) - (playerSize/2);
     p1y = p2y = initialY = lineBottom - 1;
     p1YSpeed = p2YSpeed = 0;
+
+    initialShieldRadius = p1ShieldRadius = p2ShieldRadius = playerSize * 0.85;
+    shieldRate = 0.25;
+    p1Shielding = p2Shielding = false;
 
     gravitySpeed = 2;
     jumpSpeed = 35;
@@ -68,16 +73,51 @@ function keyPressed() {
     else if (keyCode == UP_ARROW  && p2y >= initialY) jump("p2");
 }
 
+function keyReleased() {
+    if (keyCode == 83 && p1Shielding) p1Shielding = false;
+    if (keyCode == DOWN_ARROW && p2Shielding) p2Shielding = false;
+  }
+
+function activateShield() {
+    if (keyIsDown(83) && !p1Shielding) p1Shielding = true;
+    if (keyIsDown(DOWN_ARROW) && !p2Shielding) p2Shielding = true;
+}
+
+/*
+ * GAME FUNCTIONS
+*/
+
+function depleateShield() {
+    if (p1Shielding && p1ShieldRadius >= 0) p1ShieldRadius -= shieldRate;
+    else if (p1ShieldRadius <= initialShieldRadius) p1ShieldRadius += shieldRate;
+    if (p2Shielding && p2ShieldRadius >= 0) p2ShieldRadius -= shieldRate;
+    else if (p2ShieldRadius <= initialShieldRadius) p2ShieldRadius += shieldRate;
+}
+
 /*
  * DRAW FUNCTIONS
 */
 
+function drawShield() {
+    if (p1Shielding) {
+        fill(255,0,0,150);
+        noStroke();
+        circle(p1x + playerSize/2, p1y - playerSize/2, p1ShieldRadius);
+    }
+    if (p2Shielding) {
+        fill(0,0,255,150);
+        noStroke();
+        circle(p2x + playerSize/2, p2y - playerSize/2, p2ShieldRadius);
+    }
+}
+
 function drawLine() {
-    line(lineBegin, lineBottom, lineEnd, lineBottom);
     stroke(lineColor);
+    line(lineBegin, lineBottom, lineEnd, lineBottom);
 }
 
 function drawPlayers() {
+    fill(255,255,255);
     square(p1x, p1y - playerSize, playerSize);
     square(p2x, p2y - playerSize, playerSize);
 }
@@ -86,6 +126,9 @@ function draw() {
     background(50);
     drawLine();
     drawPlayers();
+    activateShield();
+    drawShield();
+    depleateShield()
     applySpeed();
     applyGravity();
 }
